397d796e94c3925cd4b023c1a375017f17bedda0
Manila Bui <manila.bui@gmail.com>
2020-02-20 15:27:16 -0600

    models created

---
+++ bangazonapi/bangazon/models/order.py
@@ -0,0 +1,21 @@
+from django.db import models
+from .customer import Customer
+from .payment_type import PaymentType
+from .product import Product
+from .order_product import OrderProduct
+
+
+class Order(models.Model):
+
+    created_at = models.DateField(auto_now=False, auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    payment_type = models.ForeignKey(PaymentType, on_delete=models.DO_NOTHING)
+    products = models.ManyToManyField(Product, through=OrderProduct)
+
+    class Meta:
+        ordering = ("created_at",)
+        verbose_name = ("order",)
+        verbose_name_plural = ("orders",)
+
+    def __str__(self):
+        return f'Paid with {self.payment_type} for {self.customer}'
