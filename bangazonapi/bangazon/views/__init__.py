eac2cc899c5f6c70a73e3bb8e637f5fc51b0238a
James Chapman <chapmanjamesg@gmail.com>
2020-02-25 10:34:04 -0600

    I added the product type

--- bangazonapi/bangazon/views/__init__.py
+++ bangazonapi/bangazon/views/__init__.py
@@ -2,3 +2,4 @@ from .product import Products
 from .order import Orders
 from .payment_type import PaymentTypes
 from .register import register_user, login_user
+from .product_type import ProductTypes
