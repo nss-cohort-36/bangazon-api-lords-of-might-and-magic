cda0413e09c0618a5968b41aa65d90720bbd2dbe
Sophia Hoffman <sophiahoffman@hotmail.com>
2020-03-04 11:41:37 -0600

    pr review

--- bangazonapi/bangazon/models/customer.py
+++ bangazonapi/bangazon/models/customer.py
@@ -7,6 +7,9 @@ class Customer(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_active = models.BooleanField()
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
